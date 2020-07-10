f9d0b83f96d234afab24478ef7bd2b6be1294241
NavrasK <trailmixerstudios@gmail.com>
2020-07-09 18:51:53 -0600

    Fixed slope behaviour with boxcasting

--- PlatformerCharacterController2D.cs
+++ PlatformerCharacterController2D.cs
@@ -1,22 +1,24 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-// TODO Fix air control
 // TODO Variable jump height
 // TODO Hitbox pinching (https://www.youtube.com/watch?v=HCnZhs-92j0)
-// TODO Improve slope handling / raycasting
+// TODO Improve slope handling / raycasting with boxcast (https://www.youtube.com/watch?v=c3iEl5AwUF8)
 // TODO One way platforms / fall through (only if grounded, and if held before becoming grounded, wait 0.2 sec)
 // TODO Moving platforms
 // TODO Determinate physics (https://www.youtube.com/watch?v=hG9SzQxaCm8)
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlatformerCharacterController2D : MonoBehaviour {
 	// Required user inputs
 	[Header("Required Values")]
-	[SerializeField] [Tooltip("Parent an empty to the bottom character and reference it here")]
-	private Transform _playerBottom;
 	[SerializeField] [Tooltip("Put ground objects in a layer and reference it here")]
 	private LayerMask _groundLayer;
+	[SerializeField] [Tooltip("Width of the area used to determine if the character is touching the ground")]
+	private float _groundedBoxWidth = 1f;
+	[Range(0.1f, 2f)] [SerializeField] [Tooltip("Height of the area used to determine if the character is touching the ground")]
+	private float _groundedBoxHeight = 0.25f;
 	[SerializeField]
 	private float _moveSpeeed = 400f;
 	[SerializeField]
@@ -34,7 +36,9 @@ public class PlatformerCharacterController2D : MonoBehaviour {
 	private float _movementSmoothing = 0.05f;
 	[SerializeField] [Tooltip("True if base sprite is facing right")]
 	private bool _spriteFacingRight = true;
-	
+	[SerializeField] [Tooltip("Show the bounding box for debugging / tweaking (make sure Gizmos are enabled)")]
+	private bool _showGizmos = false;
+
 	// Event system
 	[Header("Events")] [Space]
 	public UnityEvent OnLandEvent;
@@ -51,12 +55,17 @@ public class PlatformerCharacterController2D : MonoBehaviour {
 	const float _groundedRadius = 0.2f;
 	private bool _isGrounded;
 	private Rigidbody2D _rb;
+	private SpriteRenderer _spr;
 	private Vector3 _velocity = Vector3.zero;
 	
 	private void Awake() {
 		_rb = GetComponent<Rigidbody2D>();
-		if (_rb == null){
-			Debug.LogError("PlatformerCharacterController2D: Rigidbody2D not found");
+		_spr = GetComponent<SpriteRenderer>();
+		if (_rb == null) {
+			Debug.LogError("PlatformerCharacterController2D: Rigidbody2D component not found");
+		}
+		if (_spr == null) {
+			Debug.LogError("PlatformerCharacterController2D: SpriteRenderer component not found");
 		}
 		if (OnLandEvent == null) {
 			OnLandEvent = new UnityEvent();
@@ -93,7 +102,13 @@ public class PlatformerCharacterController2D : MonoBehaviour {
 	
 	private void CheckGround() {
 		bool wasGrounded = _isGrounded;
-		Collider2D[] colliders = Physics2D.OverlapCircleAll(_playerBottom.position, _groundedRadius, _groundLayer);
+		// Centers boxcast on bottom of sprite
+		Vector2 boxcastCenter = new Vector2(_spr.bounds.center.x, _spr.bounds.center.y - _spr.bounds.size.y/2);
+		Vector2 boxcastSize = new Vector2(_groundedBoxWidth, _groundedBoxHeight);
+		Collider2D[] colliders = Physics2D.OverlapBoxAll(boxcastCenter, boxcastSize, 0f, _groundLayer);
+		if (_showGizmos) {
+			ShowBoxGizmo(boxcastCenter, boxcastSize);
+        }
 		bool groundDetected = false;
 		// Check if it detected any collisions aside from itself
 		for (int i = 0; i < colliders.Length; i++) {
@@ -117,6 +132,24 @@ public class PlatformerCharacterController2D : MonoBehaviour {
 				_coyoteBuffer = _coyoteTime;
 			}
 		}
+	}
+
+	private void ShowBoxGizmo(Vector2 center, Vector2 size) {
+		// Show the ground bounding box
+		Color rayColour;
+		if (_isGrounded) {
+			rayColour = Color.green;
+        } else {
+			rayColour = Color.red;
+        }
+		Vector3 tl = new Vector3(center.x - size.x / 2, center.y - size.y / 2, 0);
+		Vector3 bl = new Vector3(center.x - size.x / 2, center.y + size.y / 2, 0);
+		Vector3 tr = new Vector3(center.x + size.x / 2, center.y - size.y / 2, 0);
+		Vector3 br = new Vector3(center.x + size.x / 2, center.y + size.y / 2, 0);
+		Debug.DrawLine(tl, tr, rayColour, 0);
+		Debug.DrawLine(bl, br, rayColour, 0);
+		Debug.DrawLine(tl, bl, rayColour, 0);
+		Debug.DrawLine(tr, br, rayColour, 0);
 	}
 	
 	public void Move(float dir) {
